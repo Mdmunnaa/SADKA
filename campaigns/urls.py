df5d05d19266f6e492fab4fc98d067794a966bef
Md Munna <167866775+Mdmunnaa@users.noreply.github.com>
2026-06-21 17:10:17 +0600

    Add files via upload

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, register_converter
 from . import views
+from .converters import UnicodeSlugConverter
+
+try:
+    register_converter(UnicodeSlugConverter, 'uslug')
+except ValueError:
+    pass  # Already registered
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,5 +14,5 @@ urlpatterns = [
     path('privacy/', views.privacy, name='privacy'),
     path('faq/', views.faq, name='faq'),
     path('volunteer/', views.volunteer_signup, name='volunteer_signup'),
-    path('campaigns/<slug:slug>/', views.campaign_detail, name='campaign_detail'),
+    path('campaigns/<uslug:slug>/', views.campaign_detail, name='campaign_detail'),
 ]
